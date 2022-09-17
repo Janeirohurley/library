05a1f734281f7012be37bd62604515b7f4e60792
Janeiro hurley <93239335+Janeirohurley@users.noreply.github.com>
2022-09-18 00:15:04 +0200

    Update libray.js

--- libray.js
+++ libray.js
@@ -1,7 +1,10 @@
 var amenuForGrand =document.querySelector(".option");
 var amenuForGrandBouton =document.querySelector(".amenuForGrandBouton");
 var amenuForPetitBouton =document.querySelector(".bars");
+var headerbox =document.querySelectorAll(".cont2 header");
+var headerSize =document.querySelectorAll(".cont1 header");
 
+var countsize = 3;
 amenuForGrandBouton.addEventListener("click",()=>{
     amenuForGrand.classList.toggle("left");
     
